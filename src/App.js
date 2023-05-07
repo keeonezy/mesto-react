efc8f27511e1f29b7c8fe86b5a4d985edb412c1f
Alexander <keeonenew@yandex.ru>
2023-05-07 19:28:45 +0500

    feat: add popup avatar,profile,add card

--- src/App.js
+++ src/App.js
@@ -3,14 +3,130 @@ import { Footer } from "./components/Footer";
 import { Main } from "./components/Main";
 
 function App() {
+
+  // function handleEditAvatarClick() {
+  //   setIsEditAvatarPopupOpen(true);
+  // }
+
+  // function handleAddPlaceClick() {
+  //   setIsAddPlacePopupOpen(true);
+  // }
+
+  // function handleEditProfileClick() {
+  //   setIsEditProfilePopupOpen(true);
+  // }
+
+  // function handleCardClick(card) {
+  //   setSelectedCard(card);
+  // }
+
   return (
-    <div className="body">
-      <div className="page">
-        <Header />
-        <Main />
-        <Footer />
+    <>
+      <div className="body">
+        <div className="page">
+          <Header />
+          <Main
+          // onEditAvatar={handleEditAvatarClick}
+          // onEditProfile={handleEditProfileClick}
+          // onAddPlace={handleAddPlaceClick}
+          // onCardClick={handleCardClick}
+          />
+          <Footer />
+
+          <div className="popup popup_type_avatar">
+            <div className="popup__group popup__group-avatar">
+              <button className="popup__close" type="button"></button>
+              <h2 className="popup__title">Обновить аватар</h2>
+              <form className="popup__form" name="changeAvatar" novalidate>
+                <div className="popup__section">
+                  <input type="url" placeholder="Ссылка на картинку" name="link"
+                    className="popup__input popup__input_type_url" required />
+                  <span className="popup__error"></span>
+                </div>
+                <button className="popup__button" type="submit">Сохранить</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="popup popup_type_profile">
+            <div className="popup__group">
+              <button className="popup__close" aria-label="Close" type="button"></button>
+              <h2 className="popup__title">Редактировать профиль</h2>
+              <form className="popup__form" name="editProfile" novalidate>
+                <div className="popup__section">
+                  <input type="text" placeholder="Имя" name="name" minlength="2" maxlength="40"
+                    className="popup__input popup__input_type_name" required />
+                  <span className="popup__error"></span>
+                </div>
+                <div className="popup__section">
+                  <input type="text" placeholder="Вид деятельности" name="about" minlength="2" maxlength="200"
+                    className="popup__input popup__input_type_job" required />
+                  <span className="popup__error"></span>
+                </div>
+                <button className="popup__button" type="submit">Сохранить</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="popup popup_type_card">
+            <div className="popup__group">
+              <button className="popup__close" type="button"></button>
+              <h2 className="popup__title">Новое место</h2>
+              <form className="popup__form" name="addCard" novalidate>
+                <div className="popup__section">
+                  <input type="text" placeholder="Название" name="name" minlength="2" maxlength="30"
+                    className="popup__input popup__input_type_name" required />
+                  <span className="popup__error"></span>
+                </div>
+                <div className="popup__section">
+                  <input type="url" placeholder="Ссылка на картинку" name="link"
+                    className="popup__input popup__input_type_url" required />
+                  <span className="popup__error"></span>
+                </div>
+                <button className="popup__button" type="submit">Создать</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="popup popup_type_enter">
+            <div className="popup__group">
+              <button className="popup__close" type="button"></button>
+              <h2 className="popup__title">Вы уверены?</h2>
+              <form className="popup__form" name="enter" novalidate>
+                <button className="popup__button" type="submit">Да</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="popup popup_type_image">
+            <figure className="popup__images">
+              <button className="popup__close" type="button"></button>
+              <img src="#" alt="#" className="popup__image" />
+              <figcaption className="popup__figcaption"></figcaption>
+            </figure>
+          </div>
+
+        </div>
+
+        <template className="card-li">
+          <li className="card">
+            <button className="card__trash" type="button"></button>
+            <img src="#" alt="#" className="card__image" />
+            <div className="card__info">
+              <h2 className="card__title">1</h2>
+
+              <div className="card__like-container">
+                <button className="card__like" type="button"></button>
+                <p className="card__like-number">0</p>
+              </div>
+            </div>
+          </li>
+        </template>
+
       </div>
-    </div>
+    </>
+
+
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Avatar from "../images/Avatar.jpg"
 
 function Main() {
+    function handleEditAvatarClick() {
+        const popup = document.querySelector(".popup_type_avatar");
+        popup.classList.add("popup_opened");
+    }
+    function handleEditProfileClick() {
+        const popup = document.querySelector(".popup_type_profile");
+        popup.classList.add("popup_opened");
+    }
+    const handleAddPlaceClick = () => {
+        const popup = document.querySelector(".popup_type_card");
+        popup.classList.add("popup_opened");
+    };
+
     return (
         <main className="main">
 
@@ -9,14 +22,14 @@ function Main() {
                 <div className="profile__avatar-container">
                     <img src={Avatar} alt="Фотография пользователя"
                         className="profile__avatar" />
-                    <button className="profile__button-change"></button>
+                    <button className="profile__button-change" onClick={handleEditAvatarClick}></button>
                 </div>
                 <div className="profile__info">
                     <h1 className="profile__title">Жак-Ив Кусто</h1>
                     <p className="profile__about">Исследователь океана</p>
-                    <button className="profile__button-edit" aria-label="Edit" type="button"></button>
+                    <button className="profile__button-edit" aria-label="Edit" type="button" onClick={handleEditProfileClick}></button>
                 </div>
-                <button className="profile__button-add" aria-label="Add" type="button"></button>
+                <button className="profile__button-add" aria-label="Add" type="button" onClick={handleAddPlaceClick}></button>
             </section>
 
             <section className="elements">
