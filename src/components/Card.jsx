d244ad689fa9e6fd4d34802122eb59f8a810955b
Alexander Malukhin <keeonenew@yandex.ru>
2023-05-24 21:09:58 +0300

    init: start 11 task

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,33 @@
-function Card({ card, onCardClick }) {
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+function Card({ card, onCardClick, onCardDelete, onCardLike }) {
+    const currentUser = React.useContext(CurrentUserContext);
+    const isOwn = card.owner._id === currentUser._id;
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const cardLikeButtonClassName = (
+        `element__button ${isLiked && 'element__button_active'}`
+    );
 
     const handleClick = () => {
         onCardClick(card);
     }
+    const handleDeleteClick = () => {
+        onCardDelete(card);
+    };
+    const handleLikeClick = () => {
+        onCardLike(card);
+    };
 
     return (
         <li className="card">
-            <button className="card__trash" type="button"></button>
+            {isOwn && <button className="card__trash" aria-label="удалить" onClick={handleDeleteClick} />}
             <img src={card.link} alt={card.name} onClick={handleClick} className="card__image" />
             <div className="card__info">
                 <h2 className="card__title">{card.name}</h2>
 
                 <div className="card__like-container">
-                    <button className="card__like" type="button"></button>
+                    <button className={cardLikeButtonClassName} onClick={handleLikeClick} type="button"></button>
                     <p className="card__like-number">{card.likes.length}</p>
                 </div>
             </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,38 +1,22 @@
 import React from "react";
-import { api } from "../utils/Api";
 import { Card } from "./Card";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Main({ onEditAvatar, onCardClick, onEditProfile, onAddPlace }) {
+function Main({ onEditAvatar, onCardClick, onEditProfile, onAddPlace, onCardDelete, onCardLike, cards }) {
 
-    const [userName, setUserName] = React.useState("");
-    const [userDescription, setUserDescription] = React.useState("");
-    const [userAvatar, setUserAvatar] = React.useState("");
-    const [cards, setCards] = React.useState([]);
-
-    React.useEffect(() => {
-        Promise.all([api.getInitialCards(), api.getUserInfo()])
-            .then(([card, user]) => {
-                setCards(card);
-                setUserName(user.name);
-                setUserAvatar(user.avatar);
-                setUserDescription(user.about);
-            })
-            .catch((err) => {
-                console.log(`Ошибка: ${err}`);
-            })
-    }, []);
+    const currentUser = React.useContext(CurrentUserContext);
 
     return (
         <main className="main">
 
             <section className="profile">
                 <div className="profile__avatar-container">
-                    <img src={userAvatar} alt="Фотография пользователя" className="profile__avatar" />
+                    <img src={currentUser.avatar} alt="Фотография пользователя" className="profile__avatar" />
                     <button className="profile__button-change" onClick={onEditAvatar}></button>
                 </div>
                 <div className="profile__info">
-                    <h1 className="profile__title">{userName}</h1>
-                    <p className="profile__about">{userDescription}</p>
+                    <h1 className="profile__title">{currentUser.name}</h1>
+                    <p className="profile__about">{currentUser.about}</p>
                     <button className="profile__button-edit" aria-label="Edit" type="button" onClick={onEditProfile}></button>
                 </div>
                 <button className="profile__button-add" aria-label="Add" type="button" onClick={onAddPlace}></button>
